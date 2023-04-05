e577c869ec4de4c8aa826e4bf01da0fbf8cf7b55
Griffin Minster <griffinminster@gmail.com>
2023-04-05 15:07:32 -0500

    Max token work, parameter reformatting

--- script.py
+++ script.py
@@ -1,21 +1,33 @@
 import os
 import openai
+import tiktoken
 
+
+class Params():
+    MODEL = "text-davinci-003"
+    TEMPERATURE = 0.0
 
 def main():
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.organization = os.getenv("OPENAI_ORG_KEY")
+    
+    prompt = gen_prompt()
+    
+    enc = tiktoken.encoding_for_model("text-davinci-003")
+    max_tokens = 4000 - len(enc.encode(prompt))
+    print(max_tokens)
 
-    response = openai.Completion.create(model="text-davinci-003",
+    response = openai.Completion.create(model=Params.MODEL,
                                         # prompt="Say this is a test.",
-                                        prompt=get_prompt(),
-                                        temperature=0.5,
-                                        max_tokens=300)
+                                        prompt=prompt,
+                                        temperature=Params.TEMPERATURE,
+                                        max_tokens=max_tokens)
     print(response.choices[0].text)
     return 0
 
 
-def get_prompt():
+def gen_prompt():
     source = input("Article source: ")
     prompt = f"The following is an article written by {source}. Please take \
                 the same basic information the article is presenting, but \
