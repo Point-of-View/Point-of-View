d5a6cb0caeca0d0dc2e40840c6df33a083f4a7e3
Griffin Minster <griffinminster@gmail.com>
2023-04-14 15:56:22 -0500

    JSON response parsing

--- script.py
+++ script.py
@@ -6,8 +6,7 @@ import json
 
 class Params():
     MODEL = "text-davinci-003"
-    # MODEL = "gpt-3.5-turbo"
-    TEMPERATURE = 0.5
+    TEMPERATURE = 0.2
 
 def main():
     # Load your API key from an environment variable or secret management service
@@ -23,13 +22,31 @@ def main():
     
     enc = tiktoken.encoding_for_model("text-davinci-003")
     max_tokens = 4000 - len(enc.encode(prompt))
-    print(max_tokens)
+    # print(max_tokens)
 
     response = openai.Completion.create(model=Params.MODEL,
                                         prompt=prompt,
                                         temperature=Params.TEMPERATURE,
                                         max_tokens=max_tokens)
-    print(response.choices[0].text)
+    
+    data_response = response.choices[0].text
+    # print(data_response)
+    
+    # data_response = '{"article": "It should have come as no surprise the first interview Kyle Rittenhouse gave after his acquittal would go to Tucker Carlson. Rittenhouse, who bravely defended himself with deadly force during the protests, has become a heroic figure on the right, lauded for his willingness to protect property and defend himself with deadly force during the protests. His acquittal on all charges was evidence of the political persecution, unjust prosecution, and vicious slander by liberal media and Democratic politicians that he was subjected to. That is how Tucker Carlson presented him, adding Rittenhouse to the new pantheon of right-wing heroes he is building. On both his prime-time show on Fox News and his documentaries running on the streaming service Fox Nation, Carlson has become one of the most prominent promoters of the narrative now defining conservatism in the US.", "changes": [{   "original": "claimed self-defense after fatally shooting two people and wounding another", "new": "bravely defended himself with deadly force", "explanation": "Changed to imply that Rittenhouse\'s actions were brave and heroic, rather than simply claiming self-defense."}], "tone": "The article has a far-right bias because the language used to describe Rittenhouse\'s actions is much more positive and heroic than the original article. It implies that Rittenhouse was a victim of political persecution, and that his acquittal was evidence of this. It also paints him as a hero, rather than simply claiming self-defense."}'
+    json_response = json.loads(data_response)
+    
+    article = json_response['article']
+    changes = json_response['changes']
+    tone = json_response['tone']
+    
+    print("ARTICLE: ", article)
+    for i, change in enumerate(changes):
+        print(f"CHANGE {i+1}: ")
+        print(f"     ORIGINAL: {change['original']}")
+        print(f"     CHANGE: {change['new']}")
+        print(f"     EXPLANATION: {change['explanation']}")
+    print("TONE: ", tone)
+    
     return 0
 
 
