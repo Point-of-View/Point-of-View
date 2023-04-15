efb20f92faf57b8d5b875a289af9a947832d7353
Jcheck27 <jordan.checkoff@gmail.com>
2023-04-15 18:21:43 -0500

    flask

--- script.py
+++ script.py
@@ -8,12 +8,18 @@ class Params():
     MODEL = "text-davinci-003"
     TEMPERATURE = 0.0
 
-def main():
+def translate_article(url):
+    # Scrape article text
+    article = get_article(url)
+    if not article:
+        return "Must be a Fox or CNN article"
+    
+    # Generate prompt
+    prompt = gen_prompt(article["source"], article["text"])
+
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
     openai.organization = os.getenv("OPENAI_ORG_KEY")
-    
-    prompt = gen_prompt()
     
     enc = tiktoken.encoding_for_model("text-davinci-003")
     max_tokens = 4000 - len(enc.encode(prompt))
@@ -24,16 +30,11 @@ def main():
                                         prompt=prompt,
                                         temperature=Params.TEMPERATURE,
                                         max_tokens=max_tokens)
-    print(response.choices[0].text)
-    return 0
+    
+    return response.choices[0].text
 
 
-def gen_prompt():
-    link = input("Please paste the article link: ")
-    article = get_article(link)
-    source = article["source"]
-    text = article["text"]
-
+def gen_prompt(source, text):
     prompt = f"The following is an article written by {source}. Please take \
                 the same basic information the article is presenting, but \
                 turn it into an article that would be written by "
@@ -45,7 +46,3 @@ def gen_prompt():
     
     prompt += text
     return prompt
-
-
-if __name__ == '__main__':
-    main()
