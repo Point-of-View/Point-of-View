c58bb8bdae326a10365f07286508f02c06c316b3
Jcheck27 <jordan.checkoff@gmail.com>
2023-05-01 13:37:23 -0500

    scrape route and error handling

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from script import translate_article
 from dotenv import load_dotenv
+from webscraper import get_article
 load_dotenv()
 
 app = Flask(__name__)
@@ -13,5 +14,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def translate():
     url = request.args.get('url')
     bias = request.args.get('bias')
+    if not url or not bias:
+        return "Missing parameters"
     output = translate_article(url, bias)
+    return output
+
+@app.route("/scrape")
+@cross_origin()
+def scrape():
+    url = request.args.get('url')
+    if not url:
+        return "Missing parameter"
+    output = get_article(url)
     return output
